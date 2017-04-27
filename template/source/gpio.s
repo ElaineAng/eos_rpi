6025cfab50d85a17b13bffbe0057f141f005803a
Elaine <ra1695@nyu.edu>
2017-04-27 08:10:18 -0400

    OK LED Series 3 with functions working on Pi 2

---
+++ template/source/gpio.s
@@ -0,0 +1,89 @@
+.globl GetGpioAddress
+GetGpioAddress:
+	ldr r0, =0x3F200000
+	mov pc,lr		// Return
+
+	
+.globl SetGpioFunction
+SetGpioFunction:
+	cmp r0, #53
+	cmpls r1, #7		// compare lower or same, function code vary from 0-7
+	movhi pc, lr		// move if (strictly) higher
+
+	push {lr}
+	mov r2, r0		// r2 is not used in GetGpioAddress
+
+	bl GetGpioAddress	// bl causes a branch to label,
+				// and copies the address of the next instruction into LR
+	 
+	/* When the call to GetGpioAddress returns,
+	* we now know that r0 contains the GPIO address,
+	* r1 contains the function code and r2 contains the GPIO pin number.
+	*/
+
+	functionLoop$:
+		cmp r2, #9
+		subhi r2, #10
+		addhi r0, #4
+		bhi functionLoop$
+
+	/*
+	* r2 will contain a number from 0-9
+	* r0 will contain the address in the GPIO controller of this pin's function setting
+	*/
+	add r2, r2, lsl #1	// r2 = r2 + r2 * 2 = 3 * r2
+	lsl r1, r2
+
+	mask .req r3
+	mov mask, #7
+	lsl mask, r2
+
+	mvn mask, mask
+	oldFunc .req r2
+	ldr oldFunc, [r0]
+	and oldFunc, mask
+	.unreq mask
+
+	orr r1, oldFunc
+	.unreq oldFunc
+	
+	str r1, [r0]
+	pop {pc}
+
+.globl SetGpio
+SetGpio:
+	pinNum .req r0
+	pinVal .req r1		// pinVal now become the alias of r1
+
+	cmp pinNum, #53
+	movhi pc, lr
+	push {lr}
+
+	mov r2, pinNum
+	.unreq pinNum		// .unreq remove the alias from r0
+	pinNum .req r2
+
+	bl GetGpioAddress
+	gpioAddr .req r0
+
+	pinBank .req r3
+	lsr pinBank, pinNum, #5	// pinBank = pinNum / 32, get if the pin is in group 0 or 1
+	lsl pinBank, #2
+	add gpioAddr, pinBank
+	.unreq pinBank
+
+	and pinNum, #31		// Get the remainder of pinNUM / 31 to determine which pin to set
+	setBit .req r3
+	mov setBit, #1
+	lsl setBit, pinNum
+	.unreq pinNum
+
+	teq pinVal, #0		// Test the value in pinVal against #0
+				// Updates the conditional flags on the result without placing it in any reg
+	.unreq pinVal
+	streq setBit, [gpioAddr, #0x1c] // If pinVal is 0, turn off the pin, which turn on LED (?)
+	strne setBit, [gpioAddr, #0x28] // If pinVal is not 0, turn on the pin, which turn off LED (?)
+
+	.unreq setBit
+	.unreq gpioAddr
+	pop {pc}
--- template/source/main.s
+++ template/source/main.s
@@ -18,46 +18,76 @@
 	
 .globl _start
 _start:
-	ldr r0,=0x3F200000
+	b main
+	.section .text
 
-	/*
-	* There are 54 GPIO pins, seperated into 6 groups, and there are 4 bytes for each group
-	* So there are 6*4 = 24 bytes for all GPIO pins
-	* Within a group, there are 3 bits for a pin, more specifically:
-	* 000: Input; 	001: Output;
-	* 100: Alternate Function 0;
-	* 101: A-F 1; 	110: A-F 2; 	111: A-F 3; 	011: A-F 4; 	010: A-F 5;
-	*/
-	mov r1,#1
-	lsl r1,#21 		; Pin 47 occupies the 7th pin within this group -> 7(th) * 3 (bits) = 21 (lower)
-	str r1,[r0,#0x10] 	; Pin 47 belongs to 40-49 -> GPFSEL4, the 5th set of 4 bytes
-
-	/*
-	* The output set registers are used to set a GPIO pin
-	* 0x1C is GPSET0 (responsible for pin 0-31); 0x20 is GPSET1 (responsible for pin 32-53)
-	* For any bits in these two registers:
-		0 = No effect, 1 = Set GPIO Pin n 
-	*/
-
-	mov r1,#1
-	lsl r1,#15 		; along with the first line in loop, write a 1 to GPSET1 at offset 15 
-
-loop$:
-	str r1,[r0,#0x20] 	; turn LED on
-	mov r2,#0x3F0000
-
-wait1$:
-	sub r2,#1
-	cmp r2,#0
-	bne wait1$
-
-	str r1,[r0,#0x2c] 	; 0x2c is GPCLR1, clear the previous written 1 at offset 15, turn LED off 
-	mov r2,#0x3F0000
-
-wait2$:
-	sub r2,#1
-	cmp r2,#0
-	bne wait2$
-	b loop$
-
+main:
+	mov sp, #0x8000
 	
+	/*
+	* Set the pin 47 (the LED Pin) as output (function code 1)
+	*/
+	pinNum .req r0
+	pinFunc .req r1
+
+	mov pinNum, #47
+	mov pinFunc, #1
+
+	bl SetGpioFunction
+
+	.unreq pinNum
+	.unreq pinFunc
+
+
+	loop$:
+	/*
+	* Turn the LED on
+	*/
+	pinNum .req r0
+	pinVal .req r1
+	
+	mov pinNum, #47
+	mov pinVal, #0
+	bl SetGpio
+	
+	.unreq pinNum
+	.unreq pinVal
+
+	/*
+	* Wait for sometime (for 0x3F0000 to decrease to 0)
+	*/
+	decr .req r2
+	mov decr, #0x3F0000
+
+	wait1$:
+		sub decr, #1
+		teq decr, #0	// teq does not affect V or C flag but cmp do
+		bne wait1$
+	.unreq decr
+	
+	/*
+	* Turn the LED off
+	*/
+	pinNum .req r0
+	pinVal .req r1
+
+	mov pinNum, #47
+	mov pinVal, #1
+	bl SetGpio
+
+	.unreq pinNum
+	.unreq pinVal
+
+
+	/*
+	* Wait for sometime
+	*/
+	decr .req r2
+	mov decr, #0x3F0000
+	wait2$:
+		sub decr, #1
+		teq decr, #0
+		bne wait2$
+	.unreq decr
+
+	b loop$
